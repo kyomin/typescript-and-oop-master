0a7d0f75e39622573e6127263c88cced4d8ef416
jake.kyo <jake.kyo@kakaocorp.com>
2022-09-06 16:12:29 +0900

    2-4 로딩 상태 표시

--- typescript/2-types-projects/loading.ts
+++ typescript/2-types-projects/loading.ts
@@ -23,4 +23,20 @@
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
 }
