b41a12e3470f042e3d228b7a8c33b499a485ae31
kyomin <kim031504@naver.com>
2022-11-13 16:01:50 +0900

    모션 애플리케이션 프로젝트 - Todo, Note 섹션 구현

--- motion-project/src/app.ts
+++ motion-project/src/app.ts
@@ -1,17 +1,29 @@
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
 	private readonly page: PageComponent;
 	constructor(appRoot: HTMLElement) {
+		// For Page
 		this.page = new PageComponent();
 		this.page.attatchTo(appRoot);
 
+		// For Image
 		const image = new ImageComponent(
 			'Image Title',
 			'https://picsum.photos/600/300'
 		);
 		image.attatchTo(appRoot, 'beforeend');
+
+		// For Note
+		const note = new NoteComponent('Note Title', 'Note Body');
+		note.attatchTo(appRoot, 'beforeend');
+
+		// For Todo
+		const todo = new TodoComponent('Todo Title', 'Todo Item');
+		todo.attatchTo(appRoot, 'beforeend');
 	}
 }
 
