c7635fbd6b60f24b27bedfc36df4681579e48cc8
kyomin <kim031504@naver.com>
2022-11-25 20:58:32 +0900

    모션 애플리케이션 프로젝트 - 코드 중복 제거 및 리팩토링

--- motion-project/src/app.ts
+++ motion-project/src/app.ts
@@ -1,5 +1,9 @@
 import { Component } from './components/component.js';
-import { InputDialog } from './components/dialog/dialog.js';
+import {
+	InputDialog,
+	MediaData,
+	TextData,
+} from './components/dialog/dialog.js';
 import { MediaSectionInput } from './components/dialog/input/media-input.js';
 import { TextSectionInput } from './components/dialog/input/text-input.js';
 import { ImageComponent } from './components/page/item/image.js';
@@ -12,91 +16,67 @@ import {
 	PageItemComponent,
 } from './components/page/page.js';
 
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+	new (): T;
+};
+
 class App {
 	// page는 Component와 Composable 인터페이스를 구현한 요소이다.
 	private readonly page: Component & Composable;
-	constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
+	constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
 		// For Page
 		this.page = new PageComponent(PageItemComponent);
 		this.page.attatchTo(appRoot);
 
-		const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
-		imageBtn.addEventListener('click', () => {
+		this.bindElementToDialog<MediaSectionInput>(
+			'#new-image',
+			MediaSectionInput,
+			(input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+		);
+
+		this.bindElementToDialog<MediaSectionInput>(
+			'#new-video',
+			MediaSectionInput,
+			(input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+		);
+
+		this.bindElementToDialog<TextSectionInput>(
+			'#new-note',
+			TextSectionInput,
+			(input: TextSectionInput) => new NoteComponent(input.title, input.body)
+		);
+
+		this.bindElementToDialog<TextSectionInput>(
+			'#new-todo',
+			TextSectionInput,
+			(input: TextSectionInput) => new TodoComponent(input.title, input.body)
+		);
+	}
+
+	// (MediaData | TextData) & Component의 규격을 만족하면 어떤 것이라도 받도록 T를 제한한다.
+	// MediaSectionInput과 TextSectionInput 클래스에 커플링 되지 않도록 하기 위함이다.
+	private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+		selector: string,
+		InputComponent: InputComponentConstructor<T>,
+		makeSection: (input: T) => Component
+	) {
+		const element = document.querySelector(selector)! as HTMLButtonElement;
+		element.addEventListener('click', () => {
 			const dialog = new InputDialog();
-			const inputSection = new MediaSectionInput();
-			dialog.addChild(inputSection);
-			dialog.attatchTo(dialogRoot);
+			const input = new InputComponent();
+			dialog.addChild(input);
+			dialog.attatchTo(this.dialogRoot);
 
 			dialog.setOnCloseListener(() => {
-				dialog.removeFrom(dialogRoot);
+				dialog.removeFrom(this.dialogRoot);
 			});
 			dialog.setOnSubmitListener(() => {
 				// 섹션을 만들어서 페이지에 추가해준다.
-				const image = new ImageComponent(inputSection.title, inputSection.url);
+				const image = makeSection(input);
 				this.page.addChild(image);
 
 				// 다이얼로그 창 닫아주기
-				dialog.removeFrom(dialogRoot);
-			});
-		});
-
-		const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
-		videoBtn.addEventListener('click', () => {
-			const dialog = new InputDialog();
-			const inputSection = new MediaSectionInput();
-			dialog.addChild(inputSection);
-			dialog.attatchTo(dialogRoot);
-
-			dialog.setOnCloseListener(() => {
-				dialog.removeFrom(dialogRoot);
-			});
-			dialog.setOnSubmitListener(() => {
-				// 섹션을 만들어서 페이지에 추가해준다.
-				const video = new VideoComponent(inputSection.title, inputSection.url);
-				this.page.addChild(video);
-
-				// 다이얼로그 창 닫아주기
-				dialog.removeFrom(dialogRoot);
-			});
-		});
-
-		const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
-		noteBtn.addEventListener('click', () => {
-			const dialog = new InputDialog();
-			const inputSection = new TextSectionInput();
-			dialog.addChild(inputSection);
-			dialog.attatchTo(dialogRoot);
-
-			dialog.setOnCloseListener(() => {
-				dialog.removeFrom(dialogRoot);
-			});
-			dialog.setOnSubmitListener(() => {
-				// 섹션을 만들어서 페이지에 추가해준다.
-				const note = new NoteComponent(inputSection.title, inputSection.body);
-				this.page.addChild(note);
-
-				// 다이얼로그 창 닫아주기
-				dialog.removeFrom(dialogRoot);
-			});
-		});
-
-		const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
-		todoBtn.addEventListener('click', () => {
-			const dialog = new InputDialog();
-			const inputSection = new TextSectionInput();
-			dialog.addChild(inputSection);
-			dialog.attatchTo(dialogRoot);
-
-			dialog.setOnCloseListener(() => {
-				dialog.removeFrom(dialogRoot);
-			});
-			dialog.setOnSubmitListener(() => {
-				// 섹션을 만들어서 페이지에 추가해준다.
-				const todo = new TodoComponent(inputSection.title, inputSection.body);
-				this.page.addChild(todo);
-
-				// 다이얼로그 창 닫아주기
-				dialog.removeFrom(dialogRoot);
+				dialog.removeFrom(this.dialogRoot);
 			});
 		});
 	}
